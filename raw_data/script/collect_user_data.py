02aed64ca6ed526cba3792054e80254ed63f5861
kritserv <krittipoom@outlook.com>
2024-01-06 22:57:14 +0700

    create CollectDataFromUser function

--- raw_data/script/collect_user_data.py
+++ raw_data/script/collect_user_data.py
@@ -1,12 +1,17 @@
 import requests
 
-username = "kritserv"
-
 session = requests.Session()
 #session.headers.update({"Authorization": "token {token}"})
-response = session.get("https://api.github.com/users/"+username)
-print(response)
 
-profile_data = response.json()
-followers_count = profile_data["followers"]
-print(followers_count)
+def CollectDataFromUser(username):
+    response = session.get("https://api.github.com/users/"+username)
+    print(response)
+
+    profile_data = response.json()
+    followers_count = profile_data["followers"]
+    print(followers_count)
+
+#Test function
+username_list = ["kritserv"]
+for user in username_list:
+    CollectDataFromUser(user)
