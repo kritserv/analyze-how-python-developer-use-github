bb561e4f6793f1d8a4b51941d3325e66e8e4398f
kritserv <krittipoom@outlook.com>
2024-01-07 19:26:03 +0700

    remove old data, edit script, make google search word on linkedin subdomain

--- raw_data/script/auto_scroll_for_all_google_result.py
+++ raw_data/script/auto_scroll_for_all_google_result.py
@@ -6,18 +6,25 @@ import time
 #print(pyautogui.size())
 # 1920*1080
 
-# Before Running this Script Search Google with Specific Site like linkedin or twitter and keyword for example:
-# site:linkedin.com/in "github.com/" "python developer"
+# Before Running this Script Search Google with Specific Site for example:
+# site:www.linkedin.com/in "github.com/" "python developer"
 
 # Delay Time For Changing Windows to Your Browser Before Starting Scroll Loop
 time.sleep(4)
+pyautogui.moveTo(500,900,duration=1)
+
+# Keep Scrolling to get 'More Results' Button
+for i in range(6):
+    time.sleep(0.7)
+    pyautogui.scroll(-2000)
+    time.sleep(0.7)
 
 # Move Cursor to Select 'More Results' Button
 pyautogui.moveTo(500,900,duration=1)
 
 # Click Button and Scroll Automatically, Exit with Ctrl-c.
 for i in range(300):
-    time.sleep(0.5)
+    time.sleep(0.7)
     pyautogui.click(500,900,duration=1)
     pyautogui.scroll(-2000)
 
