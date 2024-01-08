e1e8ecf8973fdddd573000e09f14c7ff62533e2d
kritserv <krittipoom@outlook.com>
2024-01-08 20:14:08 +0700

    test github event data api

--- raw_data/script/collect_user_data.py
+++ raw_data/script/collect_user_data.py
@@ -13,6 +13,12 @@ def CollectDataFromUser(username):
     followers_count = profile_data["followers"]
     print(followers_count)
 
+    response = session.get("https://api.github.com/users/"+username+"/events?per_page=100&page=1")
+    print(response)
+    event_data = response.json()
+    for event in event_data:
+        print(event['type'])
+
 #Test function
 username_list = ["kritserv"]
 for user in username_list:
